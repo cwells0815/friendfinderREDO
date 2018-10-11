ebd4feecc58fc8d3c0706a1ec67a75f4e38911ff
colin gaming <n.wells3@icloud.com>
2018-10-11 15:30:41 -0400

    redo

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# friendfinderREDO
+# Friend Finder Application
+## Description
+Friend finder express app - find a friend based on 10 questions.
---
+++ app/data/friend.js
@@ -0,0 +1,24 @@
+//======================================================================================
+// DATA
+// Below data will hold all of the completed surveys.
+//======================================================================================
+var friends = [
+    {
+        name: "Ahmed",
+        photo: "https://images.unsplash.com/photo-1534361505346-d0fadfd493b2?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=e94b853ec1f081895f21345c3a55ef01&auto=format&fit=crop&w=334&q=80`",
+        scores:[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+            ]
+    }
+];
+
+module.exports = friends;
